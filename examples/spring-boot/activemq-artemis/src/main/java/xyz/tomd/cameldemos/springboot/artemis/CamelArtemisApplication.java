300f175168be1d04072e327cc79239ae28bafd04
Tom Donohue <monodot@gmail.com>
2020-10-01 13:45:42 +0100

    Update Artemis tests - add basic Camel Spring Boot test example

--- examples/spring-boot/activemq-artemis/src/main/java/xyz/tomd/cameldemos/springboot/artemis/CamelArtemisApplication.java
+++ examples/spring-boot/activemq-artemis/src/main/java/xyz/tomd/cameldemos/springboot/artemis/CamelArtemisApplication.java
@@ -1,7 +1,12 @@
 package xyz.tomd.cameldemos.springboot.artemis;
 
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.apache.camel.component.jms.JmsComponent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.jms.ConnectionFactory;
 
 @SpringBootApplication
 public class CamelArtemisApplication {
--- examples/spring-boot/activemq-artemis/src/main/java/xyz/tomd/cameldemos/springboot/artemis/CamelArtemisRouteBuilder.java
+++ examples/spring-boot/activemq-artemis/src/main/java/xyz/tomd/cameldemos/springboot/artemis/CamelArtemisRouteBuilder.java
@@ -9,12 +9,12 @@ public class CamelArtemisRouteBuilder extends RouteBuilder {
     public void configure() throws Exception {
 
         // Send a message to a queue every 5 seconds
-        from("timer:mytimer?period=5000")
+        from("timer:mytimer?period=5000").routeId("generate-route")
                 .transform(constant("HELLO from Camel!"))
                 .to("jms:queue:INCOMING");
 
         // Receive the message from the queue and send it to another queue
-        from("jms:queue:INCOMING")
+        from("jms:queue:INCOMING").routeId("receive-route")
                 .log("Received a message - ${body} - sending to outbound queue")
                 .to("jms:queue:PROCESSED?exchangePattern=InOnly");
 
